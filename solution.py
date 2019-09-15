09727714824e224c4dfba3e7cb0bb75956b9751c
Boros93 <andrea_sequenzia@yahoo.it>
2019-09-15 19:16:44 +0200

    Add fitness cost transport
    Fitness computation finished

--- solution.py
+++ solution.py
@@ -7,14 +7,29 @@ class Solution:
         self.G = graph
 
     def compute_fitness(self):
+        demand_nodes = list()
         # Calcolo costi di istallazione facilities
         i = 0
         for u in self.G.nodes:
             if self.G.node[u]['bipartite'] == 0:
                 if(self.permutation[i] == 1): 
                     self.fitness += self.G.node[u]['f']
+                    self.G.node[u]['y'] = 1
                 i += 1
+            else:
+                demand_nodes.append(u)
+        
+        # Allocation
+        edges_cost = nx.get_edge_attributes(self.G, 'c')
+        facility = nx.get_node_attributes(self.G, 'y')
 
+        for d in demand_nodes:
+            min_cost = 10000
+            for uv in self.G.edges(d):
+                # Aggiungere condizione se f = 1
+                if edges_cost[uv[::-1]] < min_cost and facility[uv[1]]:
+                    min_cost = edges_cost[uv[::-1]]
+            self.fitness += min_cost
 
 
         
