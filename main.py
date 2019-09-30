58580f6eccb7ed66b72ff39fc8497733254ea444
Boros93 <andrea_sequenzia@yahoo.it>
2019-09-30 16:22:45 +0200

    Update latex relation

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from immune_algorithm import main_loop
 from istance_generator import load_graph
 import statistics 
 # Parametri
-generation = 2000
-population = 200# d
+generation = 3000
+population = 100# d
 clonation = 2 # dup
 mutation_rate = 0.5
 tau_b = 100
 
-graph, length_sol = load_graph('Instances/cap101.txt')
+graph, length_sol = load_graph('Instances/cap131.txt')
 
 result = []
-for i in range(0, 1):
+for i in range(0, 20):
     result.append(main_loop(graph, generation, population, clonation, mutation_rate, tau_b, length_sol))
 print("Results:", result)
 print("Mean:", statistics.mean(result))
